86c1be46d0b437778240f1c055deb386cb77793e
Divinewill Eze Okoro <codewithdiv@users.noreply.github.com>
2021-11-14 00:26:19 +0100

    Chore ⚓: Created api for displaying all tours and creating new tour

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
-
-app.get('/', (req, res) => {
-  res.status(200).send('Hello from the server!');
+app.use(express.json());
+// app.get('/', (req, res) => {
+//   res.status(200).send('Hello from the server!');
+// });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+app.get('/api/v1/tours', (req, res) => {
+  res
+    .status(200)
+    .json({ status: 'success', results: tours.length, data: { tours } });
 });
 
-const port = 3000;
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({ status: 'success', data: { tours: newTour } });
+    }
+  );
+});
+
+const port = 8000;
 app.listen(port, () => {
   console.log(`App is running on port ${port}....`);
 });
