8d866d426edf64aa6e773670e14fd04b8cc2dd2f
Divinewill Eze Okoro <codewithdiv@users.noreply.github.com>
2021-11-14 23:22:15 +0100

    Revert "Revert "Revert "Chore ⚓: Created param middleware && chained multiple middleware functions"""
    This reverts commit 7714638df1c4d1858dd3a845937c45ad783eced2.

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const app = express();
 // Node middleware
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.static(__dirname + '/public'));
 app.use((req, res, next) => {
   console.log('Hello from middleware');
   next();
