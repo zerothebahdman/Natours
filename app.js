33816c2e18ac5be2dbc4c9653e2679f1a2111a0b
Divinewill Eze Okoro <codewithdiv@users.noreply.github.com>
2021-11-14 14:01:31 +0100

    Chore ⚓: Added API endpoint to display individual tours and also patch a tour

--- app.js
+++ app.js
@@ -15,6 +15,17 @@ app.get('/api/v1/tours', (req, res) => {
     .json({ status: 'success', results: tours.length, data: { tours } });
 });
 
+// To make a variable in an endpoint nullable use the null operator in javascript (?) after the variable name e.g /:x?
+app.get('/api/v1/tours/:id/:x?', (req, res) => {
+  console.log(req.params);
+  let id = Number(req.params.id);
+  const tour = tours.find((tour) => tour.id === id);
+  if (!tour) {
+    return res.status(404).json({ status: 'error', message: 'Invalid ID' });
+  }
+  res.status(200).json({ status: 'success', data: { tour } });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
@@ -27,6 +38,22 @@ app.post('/api/v1/tours', (req, res) => {
       res.status(201).json({ status: 'success', data: { tours: newTour } });
     }
   );
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (Number(req.params.id) > tours.length) {
+    return res.status(404).json({ status: 'error', message: 'Invalid ID' });
+  }
+
+  res.status(200).json({ status: 'success', data: 'Expected Tour' });
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (Number(req.params.id) > tours.length) {
+    return res.status(404).json({ status: 'error', message: 'Invalid ID' });
+  }
+
+  res.status(204).json({ status: 'success', message: 'Tour Deleted' });
 });
 
 const port = 8000;
