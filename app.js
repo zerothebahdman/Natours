8b710a2c967f49b3835c269c24cdea2cf270b66c
Divinewill Eze Okoro <codewithdiv@users.noreply.github.com>
2021-11-14 23:24:53 +0100

    Chore ⚓: Serve static files from the server

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 // Node middleware
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(__dirname + '/public'));
 app.use((req, res, next) => {
   console.log('Hello from middleware');
   next();
