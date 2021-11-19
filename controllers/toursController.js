cd2c16eee4bd874a48fba2eb3e52c024a0fbd0c4
Divinewill Eze Okoro <codewithdiv@users.noreply.github.com>
2021-11-19 23:11:15 +0100

    Chore ⚓: Refactoring code (Created a class to handle API Features)

--- controllers/toursController.js
+++ controllers/toursController.js
@@ -1,25 +1,5 @@
 const Tour = require('../models/tourModel');
-
-// const tours = JSON.parse(
-//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-// );
-
-// exports.checkId = (req, res, next, value) => {
-//   console.log(`Tour id is: ${value}`);
-//   if (Number(req.params.id) > tours.length) {
-//     return res.status(404).json({ status: 'error', message: 'Invalid ID' });
-//   }
-// };
-
-// exports.checkBody = (req, res, next) => {
-//   if (req.body.name === undefined && req.body.price === undefined) {
-//     return res
-//       .status(400)
-//       .json({ status: 'fail', message: 'Name and Price cannot be empty' });
-//   }
-//   next();
-// };
-
+const APIFeatures = require('../utils/apiFeatures');
 // Middleware
 exports.alliasTop5Tours = (req, res, next) => {
   req.query.limit = '5';
@@ -30,49 +10,12 @@ exports.alliasTop5Tours = (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
   try {
-    // Filetering
-    const queryObj = { ...req.query };
-    const excludeKeyWords = ['sort', 'page', 'limit', 'fields'];
-    excludeKeyWords.forEach((element) => delete queryObj[element]);
-    // const query = Tour.find(queryObj);
-
-    // Advanced filtering
-    let queryString = JSON.stringify(queryObj);
-    queryString = queryString.replace(
-      /\b(gte|gt|lte|lt)\b/g,
-      (match) => `$${match}`
-    );
-    let query = Tour.find(JSON.parse(queryString));
-
-    // Sorting Record from Database
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      query.sort('-creagted_at');
-    }
-
-    //Field Limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
-    //pagination
-    const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || 100;
-    const skip = (page - 1) * limit;
-
-    query = query.skip(skip).limit(limit);
-    if (req.query.page) {
-      const numberOfTours = Tour.countDocuments();
-      if (skip >= numberOfTours) {
-        throw new Error('Page not found');
-      }
-    }
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
     res
       .status(200)
       .json({ status: 'success', results: tours.length, data: { tours } });
