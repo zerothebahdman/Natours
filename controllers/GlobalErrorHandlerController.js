d012d9603d3f0542f928b8d5c1b3476d1ea2096c
Divinewill Eze Okoro <codewithdiv@users.noreply.github.com>
2021-11-25 20:30:13 +0100

    Chore ⚓: Handling errors based on application enviroment.

--- controllers/GlobalErrorHandlerController.js
+++ controllers/GlobalErrorHandlerController.js
@@ -1,10 +1,40 @@
+const sendDevError = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    error_stack: err.stack,
+  });
+};
+
+const sendProductionError = (err, res) => {
+  //  Operational error, trusted error: send error to the client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    // programming or other unknown error: dont want to leak the details to the client
+    //  1) log the error
+    console.error('Error 💣', err);
+
+    // 2) send generic message
+    res.status(500).json({
+      status: 'fail',
+      message: `Internal server error`,
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 500;
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.APP_ENV === 'development') {
+    sendDevError(err, res);
+  } else if (process.env.APP_ENV === 'production') {
+    sendProductionError(err, res);
+  }
   next();
 };
