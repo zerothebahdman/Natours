2091e58672a941f567587f27a394cb158df5afa1
Divinewill Eze Okoro <codewithdiv@users.noreply.github.com>
2021-12-05 15:59:46 +0100

    Feature 🏋: Implemeted JWT tokenization for new users signup.

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,10 +1,19 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/UserModel');
 const AppError = require('../utils/AppError');
 
 exports.signup = async (req, res, next) => {
   try {
-    const newUser = await User.create(req.body);
-    res.status(201).json({ status: `success`, data: { newUser } });
+    const newUser = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      password_confirmation: req.body.password_confirmation,
+    });
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET_TOKEN, {
+      expiresIn: process.env.JWT_EXPIRATION,
+    });
+    res.status(201).json({ status: `success`, token, data: { newUser } });
   } catch (err) {
     return next(new AppError(err.message, err.status));
   }
