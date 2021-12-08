b81232546d2f1bf2d35d42ee870858dc2894f8a1
Divinewill Eze Okoro <codewithdiv@users.noreply.github.com>
2021-12-08 23:01:25 +0100

    Feature 🏋: Middleware for data sanitization and parameter pollution

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const rateLimit = require('express-rate-limit');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const mongoSanitizer = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
 const tourRouter = require('./routes/TourRouter');
 const userRouter = require('./routes/UserRouter');
 const AppError = require('./utils/AppErrorClass');
@@ -10,10 +14,10 @@ const GlobalErrorHandler = require('./middleware/GlobalErrorHandlerMiddleware');
 const app = express();
 // Node middleware
 
-// Sets HTTP headers
+// Middleware that sets HTTP headers
 app.use(helmet());
 
-// Logs development endpoints
+// Middleware that logs development endpoints
 if (process.env.APP_ENV === 'development') {
   app.use(morgan('dev'));
 }
@@ -28,10 +32,30 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-// Body Parser, reading data from the request body and storing it in req.body
+// Middleware that performs body Parsering, reading data from the request body and storing it in req.body
 app.use(express.json());
 
-// Servin static files
+// Middleware that performs data sanitization againt NoSQL query injection
+app.use(mongoSanitizer());
+
+// Middleware that performs data sanitization againts XXS
+app.use(xss());
+
+// Middleware that prevents parameter polution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+); //whitelist will allow us use duplicate parameters for the specified fields
+
+// Middleware that serves static files
 app.use(express.static(`${__dirname}/public`));
 // app.use((req, res, next) => {
 //   console.log('Hello from middleware');
